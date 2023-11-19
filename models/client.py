9a94abbb1be5486dcb21771bc3a30c803d84727b
Konstantin Pivnov <vaproloff@ya.ru>
2023-11-19 18:07:56 +0300

    added client model validation of DoB and document

--- models/client.py
+++ models/client.py
@@ -1,6 +1,6 @@
 import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, constr, field_validator
 
 
 class Client(BaseModel):
@@ -13,8 +13,16 @@ class Client(BaseModel):
 
 
 class ClientIn(BaseModel):
-    document: str = Field(..., title="Document", max_length=300)
+    document: constr(min_length=10, max_length=300) = Field(..., title="Document", max_length=300)
     surname: str = Field(..., title="Surname", max_length=80)
     first_name: str = Field(..., title="First Name", max_length=80)
     patronymic: str = Field(..., title="Patronymic", max_length=80)
     birthday: datetime.date = Field(..., title="Birthday")
+
+    @field_validator("birthday")
+    def validate_age(cls, value):
+        today = datetime.date.today()
+        age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+        if not 18 <= age <= 100:
+            raise ValueError("Age must be between 18 and 100.")
+        return value
